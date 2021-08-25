d33b3ef6e4bbe3c1a9b150b7f6e0860d596cf577
Scope.h <hongsch07@naver.com>
2021-08-25 13:56:28 +0900

    Updated 01-1_max3

--- Do-it-Swift.playground/Pages/01-1_max3.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/01-1_max3.xcplaygroundpage/Contents.swift
@@ -1,8 +1,18 @@
 var a, b, c: Int
 
-/*: ---
- Playground
+//: Playground에는 입력 기능이 없으므로 여기서 필요한 값을 미리 세팅
+a = 1
+b = 3
+c = 2
+// ---
 
 var max: Int
 print("Get Maximum value among 3 Integer numbers.")
 print("a: \(a)")
+print("b: \(b)")
+print("c: \(c)")
+max = a
+b > max ? max = b : ()
+c > max ? max = c : ()
+
+print("Maximum is \(max).")
