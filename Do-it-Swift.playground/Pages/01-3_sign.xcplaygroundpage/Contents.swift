148ab38a70af7f8c4646f047511a32e3e7aba3b6
Scope.h <hongsch07@naver.com>
2021-08-26 18:55:51 +0900

    Updated 01-3_sign
    sign 코드 작성 완료

--- Do-it-Swift.playground/Pages/01-3_sign.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/01-3_sign.xcplaygroundpage/Contents.swift
@@ -1,1 +1,13 @@
+var n: Int
+print("Input Integer number. : ", terminator: "")
+//MARK: - 입력이 안 되므로 따로 설정
+n = 0
+print(n)
+if n > 0 {
+    print("This is positive.")
+} else if n < 0 {
+    print("This is Negative.")
+} else {
+    print("This is zero.")
+}
 
