04845f5b8874c750f9aeb9eea1caeba35a0c594b
Scope.h <hongsch07@naver.com>
2021-08-27 07:45:45 +0900

    Updated 01-6_sum-for-pos
    repeat-while 문을 이용하여 양수만 입력받고, for문을 이용해 1부터 n까지의 합을 구한다.

--- Do-it-Swift.playground/Pages/01-6_sum-for-pos.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/01-6_sum-for-pos.xcplaygroundpage/Contents.swift
@@ -1,1 +1,17 @@
+var n : Int
+var sum : Int
+var index = 0 // 입력을 위한 처리
+print("Get Sum of 1 to n.")
+repeat {
+    print("n : ", terminator: "")
+    let data = [-4, 6, 7] // 입력을 위한 처리
+    n = data[index] // 입력을 위한 처리
+    print(n) // 입력을 위한 처리
+    index += 1
+} while n <= 0
+sum = 0
+for i in 1...n {
+    sum += i
+}
+print("Sum of 1 to \(n) = \(sum)")
 
