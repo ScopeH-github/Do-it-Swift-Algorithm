64be516efabdd040f34cfeb26925a37d98a5ab29
Scope.h <hongsch07@naver.com>
2021-08-29 03:06:47 +0900

    [UPDATE] 01-1c-2 dbl_digits

--- Do-it-Swift.playground/Pages/01-1c-2_dbl-digits.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/01-1c-2_dbl-digits.xcplaygroundpage/Contents.swift
@@ -1,1 +1,12 @@
+var no : Int
+let sample = [123, 3, 1923, 12]
+var i = 0
 
+print("Input double digits number")
+repeat {
+    print("number: ", terminator: "")
+    no = sample[i]
+    i += 1
+    print(no)
+} while no < 10 || no > 99
+print("no : \(no)")
