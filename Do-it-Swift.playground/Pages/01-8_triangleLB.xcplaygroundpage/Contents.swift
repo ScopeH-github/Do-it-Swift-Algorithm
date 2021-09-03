1b8eee8e16bc212cbc8da095801304332c6a3a2a
Scope.h <hongsch07@naver.com>
2021-09-04 01:36:53 +0900

    [UPDATE] 01-8 triangleLB
    01-8 문제 추가

--- Do-it-Swift.playground/Pages/01-8_triangleLB.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/01-8_triangleLB.xcplaygroundpage/Contents.swift
@@ -1,1 +1,17 @@
+var n: Int
+
+repeat {
+    print("How much layers do you want : ", terminator: "")
+
+    n = 5
+    print(n)
+    
+} while n <= 0
+
+for i in 1...n {
+    for _ in 1...i {
+        print("*", terminator: "")
+    }
+    print()
+}
 
