5e8b8003948f82313a60db57b16205dd6b18bf55
Scope.h <hongsch07@naver.com>
2021-09-04 03:08:32 +0900

    [UPDATE] 01-Ex Q16
    Q16 - 01-8에서 진행한 내용을 함수로 분리하는 문제

--- Do-it-Swift.playground/Pages/01-8_triangleLB.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/01-8_triangleLB.xcplaygroundpage/Contents.swift
@@ -10,7 +10,7 @@ repeat {
 
 for i in 1...n {
     for _ in 1...i {
-        print("*", terminator: "")
+        print("*", terminator: "  ")
     }
     print()
 }
--- Do-it-Swift.playground/Pages/01-Ex.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/01-Ex.xcplaygroundpage/Contents.swift
@@ -251,3 +251,23 @@ for i in 1...height {
     print()
 }
 print()
+
+//MARK: - Q16
+var n: Int
+
+repeat {
+    print("How much layers do you want : ", terminator: "")
+    
+    n = 5
+    print(n)
+    
+} while n <= 0
+
+for i in 1...n {
+    for _ in 1...i {
+        print("*", terminator: "  ")
+    }
+    print()
+}
+
+
