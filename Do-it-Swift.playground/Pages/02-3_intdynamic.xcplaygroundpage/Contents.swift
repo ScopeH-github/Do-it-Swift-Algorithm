5850ad13db2498c4f16a64b8a8d95c7048e44a98
Scope.h <hongsch07@naver.com>
2021-09-05 10:24:51 +0900

    [UPDATE] 02-3 intdynamic
    Swift에서 포인터를 사용한 예제.
    이것과 다음 02-4를 제외하고는 포인터를 사용하지 않을 것.

--- Do-it-Swift.playground/Pages/02-3_intdynamic.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-3_intdynamic.xcplaygroundpage/Contents.swift
@@ -1,1 +1,15 @@
+let x = UnsafeMutablePointer<Int>.allocate(capacity: MemoryLayout.size(ofValue: Int()))
+defer {
+    x.deallocate()
+}
+if x == nil {
+    print("Allocation Failed.")
+} else {
+    x.pointee = 57
+    print("*x = \(x.pointee)")
+}
 
+// Swift에서는 기본적으로 배열은 동적 할당으로 이루어짐
+// Swift애서는 포인터 사용을 권장하지 않음
+// 이는 Swift에서 optional을 제공하는 이유와 같음
+// 예상치 못한 동작을 방지하기 위함
