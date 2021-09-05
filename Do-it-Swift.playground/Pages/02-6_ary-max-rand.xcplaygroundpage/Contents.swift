9c642a1a4f7c95325c12e14abba0196c9e2ecf83
Scope.h <hongsch07@naver.com>
2021-09-05 21:52:23 +0900

    [UPDATE] 02-6 ary-max-rand 환료
    난수를 사용해 임의의 데이터 할당하기

--- Do-it-Swift.playground/Pages/02-6_ary-max-rand.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-6_ary-max-rand.xcplaygroundpage/Contents.swift
@@ -1,1 +1,19 @@
+func maxOf(array: [Int]) -> Int {
+    var max = array[0]
+    for i in 1..<array.count {
+        array[i] > max ? max = array[i] : ()
+    }
+    return max
+}
+var height: [Int] = []
+var number: Int
 
+print("Number of people :", terminator: " ")
+number = 5
+print(number)
+print("Enter height of each \(number) people.")
+for i in 0..<number {
+    height.append(Int.random(in: 100...189))
+    print("height[\(i)] : \(height[i]) ")
+}
+print("Max value is \(maxOf(array: height)).")
