cfdfb7cfca15e14fd5eb4e8c1fe0fdc9e7c0a509
Scope.h <hongsch07@naver.com>
2021-09-05 20:31:06 +0900

    [UPDATE] 02-5 ary-max 완료
    최댓값을 구하는 과정을 maxOf 함수로 구현

--- Do-it-Swift.playground/Pages/02-5_ary-max.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-5_ary-max.xcplaygroundpage/Contents.swift
@@ -1,1 +1,23 @@
+func maxOf(array: [Int]) -> Int {
+    var max = array[0]
+    for i in 1..<array.count {
+        array[i] > max ? max = array[i] : ()
+    }
+    return max
+}
+
+let sampledata = [172, 153, 192, 140, 165]
+var height: [Int] = []
+var number: Int
+
+print("Number of people :", terminator: " ")
+number = 5
+print(number)
+print("Enter height of each \(number) people.")
+for i in 0..<number {
+    print("height[\(i)] : ", terminator : "")
+    height.append(sampledata[i])
+    print(sampledata[i])
+}
+print("Max value is \(maxOf(array: height)).")
 
