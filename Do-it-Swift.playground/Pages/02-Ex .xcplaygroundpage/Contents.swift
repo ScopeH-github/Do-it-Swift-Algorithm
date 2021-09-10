ac9cb4dd584d9b319303f8d76be4bb68464db4e4
Scope.h <hongsch07@naver.com>
2021-09-11 01:49:31 +0900

    [UPDATE] 02-Ex Q2 완료
    합 구하기

--- Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 //MARK: - Q1
-func maxOf(array: [Int]) -> Int {
+func minOf(array: [Int]) -> Int {
     var min = array[0]
     for i in 1..<array.count {
         array[i] < min ? min = array[i] : ()
@@ -8,16 +8,41 @@ func maxOf(array: [Int]) -> Int {
 }
 
 var height1 :  [Int] = []
-var number: Int
+var number1: Int
 
 print("Number of people :", terminator: " ")
-number = 5
-print(number)
-print("Enter height of each \(number) people.")
-for i in 0..<number {
+number1 = 5
+print(number1)
+print("Enter height of each \(number1) people.")
+for i in 0..<number1 {
     print("height[\(i)] : ", terminator : "")
     height1.append(Int.random(in: 130...190))
     print(height1[i])
 }
-print("Minimum value is \(maxOf(array: height1)).")
+print("Minimum value is \(minOf(array: height1)).")
+print()
 
+//MARK: - Q2
+func sumOf(array: [Int]) -> Int {
+    var sum = array[0]
+    for i in 1..<array.count {
+        sum += array[i]
+    }
+    return sum
+}
+
+var height2 :  [Int] = []
+var number2: Int
+
+print("Number of people :", terminator: " ")
+number2 = 5
+print(number2)
+print("Enter height of each \(number2) people.")
+for i in 0..<number2 {
+    print("height[\(i)] : ", terminator : "")
+    height2.append(Int.random(in: 130...190))
+    print(height2[i])
+}
+print("Minimum value is \(sumOf(array: height2)).")
+
+//MARK: - Q3
