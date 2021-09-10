4c0883226cc47926777d35a4c4996d08628022d5
Scope.h <hongsch07@naver.com>
2021-09-10 21:59:40 +0900

    [UPDATE] 02-Ex 01해결
    Q1 해결

--- Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
@@ -1,1 +1,23 @@
-//
+//MARK: - Q1
+func maxOf(array: [Int]) -> Int {
+    var min = array[0]
+    for i in 1..<array.count {
+        array[i] < min ? min = array[i] : ()
+    }
+    return min
+}
+
+var height1 :  [Int] = []
+var number: Int
+
+print("Number of people :", terminator: " ")
+number = 5
+print(number)
+print("Enter height of each \(number) people.")
+for i in 0..<number {
+    print("height[\(i)] : ", terminator : "")
+    height1.append(Int.random(in: 130...190))
+    print(height1[i])
+}
+print("Minimum value is \(maxOf(array: height1)).")
+
