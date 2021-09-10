feba5708d3374445717553d7466810996901b161
Scope.h <hongsch07@naver.com>
2021-09-10 19:35:36 +0900

    [UPDATE] 02-8B card-conv 풀이
    기수 변환 프로그램 완성

--- Do-it-Swift.playground/Pages/02-8_card-conv.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-8_card-conv.xcplaygroundpage/Contents.swift
@@ -6,10 +6,42 @@ func cardConvert(x: inout UInt, n: Int, _ d: inout [Character]) -> Int {
         digits += 1
     } else {
         while x != 0 {
-            d[digits] = dchar[dchar.index(dchar.startIndex, offsetBy: Int(x) % n)]
+            d.append(dchar[dchar.index(dchar.startIndex, offsetBy: Int(x) % n)])
             digits += 1
             x /= UInt(n)
         }
     }
     return digits
 }
+
+var number: UInt
+var cd: Int
+var digitno: Int
+var cno: [Character] = []
+var retry: Bool = false
+
+print("Convert radix from decimal.")
+repeat {
+    number = UInt.random(in: 0...999)
+    let entered = number
+    print("Number(non-negative): \(number)")
+    repeat {
+        cd = Int.random(in: 1...40)
+        print("Radix (2-36): \(cd)")
+    } while cd < 2 || cd > 36
+    digitno = cardConvert(x: &number, n: cd, &cno)
+    print("\n\(entered) is ", terminator : "")
+    for i in stride(from: digitno-1, to: -1, by: -1) {
+        print(cno[i], terminator: "")
+    }
+    print(" in radix \(cd).\n")
+    let answer = Int.random(in: 0...1)
+    cno.removeAll()
+    print("Retry? (1-Yes / 0-No) : \(answer)")
+    if answer == 0 {
+        retry = false
+    } else {
+        retry = true
+    }
+    print("\n")
+} while retry
