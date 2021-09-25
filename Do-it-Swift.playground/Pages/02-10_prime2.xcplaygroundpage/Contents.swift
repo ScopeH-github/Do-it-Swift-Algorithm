59c79a8096663b9dc03fd77c5a2faa0d4de5469c
Scope.h <hongsch07@naver.com>
2021-09-25 20:09:59 +0900

    [UPDATE] 02-10 prime2
    소수 배열을 사용하여 알고리즘 최적화 1

--- Do-it-Swift.playground/Pages/02-10_prime2.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-10_prime2.xcplaygroundpage/Contents.swift
@@ -1,1 +1,27 @@
+var i, n: Int
+var prime: [Int] = []
+var ptr = 0
+var counter: UInt = 0
 
+prime.append(2)
+ptr += 1
+n = 3
+while n <= 1000 {
+    i = 1
+    while i < ptr {
+        counter += 1
+        if n % prime[i] == 0 {
+            break
+        }
+        i += 1
+    }
+    if ptr == i {
+        prime.append(n)
+        ptr += 1
+    }
+    n += 2    // only check odd number
+}
+for i in 0..<ptr {
+    print(prime[i])
+}
+print("number of division: \(counter)")
