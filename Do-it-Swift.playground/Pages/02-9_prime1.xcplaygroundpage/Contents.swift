9ca3dab3002620a56ed46f1767dd694968a6de15
Scope.h <hongsch07@naver.com>
2021-09-25 19:51:19 +0900

    [UPDATE] 02-9 prime1
    소수 구하기 1
    
    
    (swift의 경우 for문은 변수가 밖으로 빠지지 않아 while을 사용)

--- Do-it-Swift.playground/Pages/02-9_prime1.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-9_prime1.xcplaygroundpage/Contents.swift
@@ -1,1 +1,21 @@
+var i, n: Int
+var counter: UInt = 0
+
+n = 2
+i = 2
+while n <= 1000 {
+    while i < n {
+        counter += 1
+        if n % i == 0 {
+            break
+        }
+        i += 1
+    }
+    if n == i {
+        print(n)
+    }
+    n += 1
+    i = 2
+}
+print("number of division: \(counter)")
 
