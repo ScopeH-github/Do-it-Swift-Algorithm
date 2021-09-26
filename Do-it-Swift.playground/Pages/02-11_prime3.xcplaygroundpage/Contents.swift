4f7f64cdfe5f69bce53b33b32f66f0cc208441b7
Scope.h <hongsch07@naver.com>
2021-09-26 15:30:29 +0900

    [UPDATE] 02-11 prime3
    제곱근을 이용하여 연산 횟수를 줄임

--- Do-it-Swift.playground/Pages/02-11_prime3.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-11_prime3.xcplaygroundpage/Contents.swift
@@ -1,1 +1,30 @@
+var i, n: Int
+var prime = [2]
+prime.append(3)
+var counter: UInt = 0
+    
+n = 5
+while n <= 1000 {
+    var flag = false
+    i = 1
+    counter += 1
+    while prime[i] * prime[i] <= n {
+        counter += 1
+        if n % prime[i] == 0 {
+            flag = true
+            break
+        }
+        counter += 1
+        i += 1
+    }
+    if !flag {
+        prime.append(n)
+    }
+    
+    n += 2
+}
 
+for i in prime {
+    print(i)
+}
+print("Number of mul and div operation: \(counter)")
