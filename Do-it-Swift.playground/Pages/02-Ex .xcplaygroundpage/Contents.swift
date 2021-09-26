7242681ddd319fd9cc6cfe5c6c296e5e7f92eb00
Scope.h <hongsch07@naver.com>
2021-09-26 16:29:51 +0900

    [UPDATE] 02-Ex Q11 추가
    dayofyear 함수를 i와 days 없이 while로 구현할 것

--- Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
@@ -324,3 +324,53 @@ print("before a = \(a)")
 shuffle(a: &a)
 print("after a = \(a)")
 print("after apple_shuffle a -> b = \(b)")
+print()
+
+// MARK: - Q11
+let mdays = [
+    [31,28,31,30,31,30,31,31,30,31,30,31],
+    [31,29,31,30,31,30,31,31,30,31,30,31]
+]
+
+var year, month, day: Int
+var retry = false
+repeat {
+    year = Int.random(in: 1900...2099)
+    month = Int.random(in: 1...12)
+    day = Int.random(in: 1...31)
+    if month == 2 {
+        if (day > 28 && isLeap(year: year) == 0) {
+            day = 28
+        } else if (day > 29 && isLeap(year: year) == 1) {
+            day = 29
+        } 
+    } else if month == 4 || month == 6 || month == 9 || month == 11 {
+        if day > 30 {
+            day = 30
+        }
+    }
+    print("year: \(year)")
+    print("month: \(month)")
+    print("day: \(day)")
+    
+    print("=> \(dayOfYear(y: year, m: month, d: day)) day of \(year)")
+    
+    let input = Int.random(in: 0...1)
+    retry = input == 1
+    print("Retry? (1 - Yes / 0 - No) : \(input)")
+} while retry
+print()
+
+func isLeap(year: Int) -> Int {
+    year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? 1 : 0
+}
+
+func dayOfYear(y: Int, m: Int, d: Int) -> Int {
+    var partMdays: [Int] = []
+    var month = 0
+    while month < m - 1 {
+        partMdays.append(mdays[isLeap(year: y)][month])
+        month += 1
+    }
+    return partMdays.reduce(0, { $0 + $1 }) + d
+}
