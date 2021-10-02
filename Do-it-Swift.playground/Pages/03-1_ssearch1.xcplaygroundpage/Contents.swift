98fa51f132e70397fc8de421a66a29fc1f831f67
Scope.h <hongsch07@naver.com>
2021-10-02 21:28:06 +0900

    [UPDATE] 03_1 ssearch1 완료
    선형검색 알고리즘 1

--- Do-it-Swift.playground/Pages/03-1_ssearch1.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/03-1_ssearch1.xcplaygroundpage/Contents.swift
@@ -1,1 +1,37 @@
+func search(a: [Int], key: Int) -> Int? {
+    let n = a.count
+    var i = 0
+    while true {
+        if i == n {
+            return nil
+        }
+        if a[i] == key {
+            return i
+        }
+        i += 1
+    }
+}
 
+var nx, ky: Int
+var idx: Int?
+print("linear search")
+let nxInput = Int.random(in: 4...8)
+nx = nxInput
+print("Count of elements: \(nx)")
+var x : [Int] = []
+for i in 0..<nx {
+    let elInput = Int.random(in: 0...15)
+    x.append(elInput)
+    print("x[\(i)] = \(x[i])")
+}
+let kyInput = Int.random(in: 0...10)
+ky = kyInput
+print("Search Key : \(ky)")
+idx = search(a: x, key: ky)
+if idx == nil {
+    print("Search Failed.")
+} else {
+    if let index = idx {
+        print("\(ky) is at x[\(index)].")
+    }
+}
