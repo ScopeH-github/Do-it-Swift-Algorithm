cc909b75b5decad8c0dd33a04912e645d52ab141
Scope.h <hongsch07@naver.com>
2021-11-03 17:07:24 +0900

    [ UPDATE ] 02-Ex 부분 분리 완료

--- Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/02-Ex .xcplaygroundpage/Contents.swift
@@ -1,98 +1,118 @@
 //MARK: - Q1
-func minOf(array: [Int]) -> Int {
-    var min = array[0]
-    for i in 1..<array.count {
-        array[i] < min ? min = array[i] : ()
+func Q1() {
+    
+    func minOf(array: [Int]) -> Int {
+        var min = array[0]
+        for i in 1..<array.count {
+            array[i] < min ? min = array[i] : ()
+        }
+        return min
     }
-    return min
+    
+    var height :  [Int] = []
+    var number: Int
+    
+    print("Number of people :", terminator: " ")
+    number = 5
+    print(number)
+    print("Enter height of each \(number) people.")
+    for i in 0..<number {
+        print("height[\(i)] : ", terminator : "")
+        height.append(Int.random(in: 130...190))
+        print(height[i])
+    }
+    print("Minimum value is \(minOf(array: height)).")
 }
 
-var height1 :  [Int] = []
-var number1: Int
-
-print("Number of people :", terminator: " ")
-number1 = 5
-print(number1)
-print("Enter height of each \(number1) people.")
-for i in 0..<number1 {
-    print("height[\(i)] : ", terminator : "")
-    height1.append(Int.random(in: 130...190))
-    print(height1[i])
-}
-print("Minimum value is \(minOf(array: height1)).")
+Q1()
 print()
 
 //MARK: - Q2
-func sumOf(array: [Int]) -> Int {
-    var sum = array[0]
-    for i in 1..<array.count {
-        sum += array[i]
+func Q2() {
+    
+    func sumOf(array: [Int]) -> Int {
+        var sum = array[0]
+        for i in 1..<array.count {
+            sum += array[i]
+        }
+        return sum
     }
-    return sum
+    
+    var height :  [Int] = []
+    var number: Int
+    
+    print("Number of people :", terminator: " ")
+    number = 5
+    print(number)
+    print("Enter height of each \(number) people.")
+    for i in 0..<number {
+        print("height[\(i)] : ", terminator : "")
+        height.append(Int.random(in: 130...190))
+        print(height[i])
+    }
+    print("Sum of values is \(sumOf(array: height)).")
 }
 
-var height2 :  [Int] = []
-var number2: Int
-
-print("Number of people :", terminator: " ")
-number2 = 5
-print(number2)
-print("Enter height of each \(number2) people.")
-for i in 0..<number2 {
-    print("height[\(i)] : ", terminator : "")
-    height2.append(Int.random(in: 130...190))
-    print(height2[i])
-}
-print("Sum of values is \(sumOf(array: height2)).")
+Q2()
 print()
 
 //MARK: - Q3
-func aveOf(array: [Int]) -> Double {
-    var ave = Double(array[0])
-    for i in 1..<array.count {
-        ave += Double(array[i])
+func Q3() {
+    
+    func aveOf(array: [Int]) -> Double {
+        var ave = Double(array[0])
+        for i in 1..<array.count {
+            ave += Double(array[i])
+        }
+        ave /= Double(array.count)
+        return ave
     }
-    ave /= Double(array.count)
-    return ave
+    
+    var height :  [Int] = []
+    var number: Int
+    
+    print("Number of people :", terminator: " ")
+    number = 5
+    print(number)
+    print("Enter height of each \(number) people.")
+    for i in 0..<number {
+        print("height[\(i)] : ", terminator : "")
+        height.append(Int.random(in: 130...190))
+        print(height[i])
+    }
+    print("Average of values is \(aveOf(array: height)).")
 }
 
-var height3 :  [Int] = []
-var number3: Int
-
-print("Number of people :", terminator: " ")
-number3 = 5
-print(number3)
-print("Enter height of each \(number3) people.")
-for i in 0..<number3 {
-    print("height[\(i)] : ", terminator : "")
-    height3.append(Int.random(in: 130...190))
-    print(height3[i])
-}
-print("Average of values is \(aveOf(array: height3)).")
+Q3()
 print()
 
 //MARK: - Q4
-func maxOf(array: [Int]) -> Int {
-    var max = array[0]
-    for i in 1..<array.count {
-        array[i] > max ? max = array[i] : ()
+func Q4() {
+    
+    func maxOf(array: [Int]) -> Int {
+        var max = array[0]
+        for i in 1..<array.count {
+            array[i] > max ? max = array[i] : ()
+        }
+        return max
     }
-    return max
+    
+    var height : [Int] = []
+    var number: Int
+    
+    print("Number of people :", terminator: " ")
+    number = Int.random(in: 5...20)
+    print(number)
+    print("Enter height of each \(number) people.")
+    for i in 0..<number {
+        print("height[\(i)] : ", terminator : "")
+        height.append(Int.random(in: 130...190))
+        print(height[i])
+    }
+    print("Max of values is \(maxOf(array: height)).")
 }
 
-var height4 : [Int] = []
-var number4: Int
-
-print("Number of people :", terminator: " ")
-number4 = Int.random(in: 5...20)
-print(number4)
-print("Enter height of each \(number4) people.")
-for i in 0..<number4 {
-    print("height[\(i)] : ", terminator : "")
-    height4.append(Int.random(in: 130...190))
-    print(height4[i])
-}
-print("Max of values is \(maxOf(array: height4)).")
+Q4()
 print()
 
 //  MARK: - Q5
