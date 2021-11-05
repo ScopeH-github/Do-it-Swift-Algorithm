6656839d95b542baad0b47a174c7fbcf1e4b7bc1
Scope.h <hongsch07@naver.com>
2021-11-05 16:22:28 +0900

    [UPDATE] 03-Ex_Q1 풀이시작
    예제 실행 전용 함수를 mrl.swift에 추가

--- Do-it-Swift.playground/Pages/03-Ex.xcplaygroundpage/Contents.swift
+++ Do-it-Swift.playground/Pages/03-Ex.xcplaygroundpage/Contents.swift
@@ -1,1 +1,40 @@
+func Q1() {
+    func search(a: [Int], key: Int) -> Int? {
+        var i = 0
+        var aTemp = a
+        aTemp.append(key)
+        while true {
+            if aTemp[i] == key {
+                break
+            }
+            i += 1
+        }
+        return i == a.count ? nil : i
+    }
+    
+    var nx, ky: Int
+    var idx: Int?
+    print("sentinel linear search")
+    let nxInput = Int.random(in: 4...8)
+    nx = nxInput
+    print("Count of elements: \(nx)")
+    var x : [Int] = []
+    for i in 0..<nx {
+        let elInput = Int.random(in: 0...15)
+        x.append(elInput)
+        print("x[\(i)] = \(x[i])")
+    }
+    let kyInput = Int.random(in: 0...10)
+    ky = kyInput
+    print("Search Key : \(ky)")
+    idx = search(a: x, key: ky)
+    if idx == nil {
+        print("Search Failed.")
+    } else {
+        if let index = idx {
+            print("\(ky) is at x[\(index)].")
+        }
+    }
+}
 
+runEx(Q: 1, Q1())
--- Do-it-Swift.playground/Sources/mrl.swift
+++ Do-it-Swift.playground/Sources/mrl.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public func readLine(_ input: [String], _ currentLine: inout Int) -> String? {
     if currentLine < input.endIndex {
         let line = input[currentLine]
@@ -6,4 +8,10 @@ public func readLine(_ input: [String], _ currentLine: inout Int) -> String? {
     } else {
         return nil
     }
+}
+
+public func runEx(Q: Int, _ code: () -> ()) {
+    print("====== Q\(String(format: "%2d", Q)) ======")
+    code
+    print()
 }
